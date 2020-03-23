0934e0213050788f01f643f1e7cd40a8756067e0
Mohammed Youssef <m.youssef.soliman@gmail.com>
2020-03-23 05:10:20 +0200

    including lifeCylce method to Modal component

--- src/components/UI/modal/Modal.js
+++ src/components/UI/modal/Modal.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './modal.css';
 
 import Backdrop from '../backdrop/backdrop'
 
-const Modal = props => {
-    return (
-        <>
-        <Backdrop close={props.close} show={props.show}/>
-        <div className={classes.Modal}
-            style = {{
-                transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-                opacity: props.show ? "1" : "0"
-            }}
-        >
-            {props.children}
-        </div>
-        </>
-    );
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps,nextState) {
+
+        return nextProps.show !== this.props.show
+    }
+
+    componentWillUpdate() {
+        console.log('[OrderSummery should update]')
+    }
+
+    render() {
+
+        return (
+            <>
+            <Backdrop close={this.props.close} show={this.props.show}/>
+            <div className={classes.Modal}
+                style = {{
+                    transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+                    opacity: this.props.show ? "1" : "0"
+                }}
+            >
+                {this.props.children}
+            </div>
+            </>
+        );
+    }
 }
 
 export default Modal;
